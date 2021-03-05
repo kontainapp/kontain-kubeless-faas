88004454e9d7343fbe03db5e2f37cf22b0a06f2e
John Muth <muth@kontain.app>
2021-03-04 16:47:08 -0800

    Fix output format

--- kontain-faas/server/test_funcs/test_func_km.c
+++ kontain-faas/server/test_funcs/test_func_km.c
@@ -2,8 +2,11 @@
  * usage: test1 
  */
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/syscall.h>
 
 char output_buffer[4096];
@@ -12,12 +15,18 @@ char output_buffer[4096];
 #define HC_snapshot_getdata 504
 #define HC_snapshot_putdata 503
 
+char outbuf[4096];
 int
 main(int argc, char *argv[])
 {
   char *msg = getenv("FUNC_MSG");
-  printf("%s\n", msg);
-  int msglen = strlen(msg);
+  if (msg == NULL) {
+    fprintf(stderr, "Env variable FUNC_MSG does not exist\n");
+    return 1;
+  }
+  snprintf(outbuf, sizeof(outbuf), "STATUSCODE: 200\nDATA: %s\n", msg);
+  printf("%s", outbuf);
+  int msglen = strlen(outbuf);
   syscall(HC_snapshot_putdata, msg, msglen);
   return 0;
 }
