b953975666299c06181b7acdad7966e5a6db2c3f
Paul Popelka <paulp@kontain.app>
2021-03-23 13:11:14 -0700

    Address review comments.

--- kontain-faas/server/build-oci-image-tool.bash
+++ kontain-faas/server/build-oci-image-tool.bash
@@ -8,9 +8,7 @@ fi
 rm -f go.mod go.sum
 
 go get -d github.com/opencontainers/image-tools/cmd/oci-image-tool
-pushd $GOPATH/src/github.com/opencontainers/image-tools/
-make tool
-popd
+make -C $GOPATH/src/github.com/opencontainers/image-tools tool
 
 # The oci-image-tool executable should be $GOPATH/src/github.com/opencontainers/image-tools/oci-image-tool
 cp $GOPATH/src/github.com/opencontainers/image-tools/oci-image-tool .
